705d412ddd65f6828d35a19e5db17941617d8bb0
eddieelorza <81988834+eddieelorza@users.noreply.github.com>
2021-09-22 22:31:19 -0500

    modificando nuevos archivos

--- FRONT/js/app.js
+++ FRONT/js/app.js
@@ -1,15 +1,33 @@
-//Barra de navegacion
-// document.querySelectorAll('.navbar .nav-link').forEach(enlace => {
-//     enlace.addEventListener('click', function (e) {
-//         e.preventDefault();
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
 
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth',
-//             block: 'start'
-//         });
-//     });
-// });
-//scroll Navbr
 window.onscroll = function () {
     let scroll = window.scrollY;
 
@@ -19,20 +37,33 @@ window.onscroll = function () {
     } else {
         headerScroll.classList.remove('bg-success');
     }
+
+	if(document.documentElement.scrollTop>100){
+		document.querySelector('.go--top-container').classList.add('show')
+	}else{
+		document.querySelector('.go--top-container').classList.remove('show')
+	}
+
 };
+document.querySelector('.go--top-container').addEventListener('click', () =>{
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	})
+})
 
 
 //
 $(document).ready(function(){
     $('#Sushi').click(function(){
-       $("#contenido-menu").load("../views/menus/sushi.html");
+       $("#contenido-menu").load("/hamburguesas.html");
     });
 
     $('#Hamburguesas').click(function(){
-       $("#contenido-menu").load("../views/menus/Hamburguesas.html");
+       $("#contenido-menu").load("/hamburguesas.html");
     });
 
     $('#Pizzas').click(function(){
-       $("#contenido-menu").load("../views/menus/Pizzas.html");
+       $("#contenido-menu").load("/hamburguesas.html");
     });
 });
